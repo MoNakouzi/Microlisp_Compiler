f772fe105194b28e136ba00a151dd0d099b44c90
Mo Nakouzi <mnakouzi@ryerson.ca>
2023-02-07 19:40:49 -0500

    Added the microlisp compiler

---
+++ MicroLisp Compiler.lisp
@@ -0,0 +1,95 @@
+(defun evaluate (e env)
+  (let ((res (valid-args? e)))
+    (if (car res)
+        (if (atom e)
+            (cond
+              ((constant? e) e)
+              ((symbol? e) (lookup e env))
+              ((or (procedure? e) (numberp e)) e)
+              (t (wrong "Could not evaluate expression: ~S~%" e)))
+            (case (car e)
+              ((progn) (eprogn (cdr e) env))
+              ((if) (if-check e env))
+              ((let) (let-init e env))
+              ((setf) (update! (cadr e) env (evaluate (caddr e) env)))
+              (t (invoke (evaluate (car e) env) (evlist (cdr e) env)))))
+        (format t "Invalid number of args: expected ~d, got ~d in ~S~%" (caddr res) (cadr res) e))))
+
+(defun if-check (e env)
+  (if (not (equal (evaluate (second e) env) nil))
+      (evaluate (third e) env)
+      (evaluate (cadddr e) env)))
+
+(defun let-init (e env &optional (alist '()))
+  (dolist (i (second e))
+    (if (typep (cadr i) 'cons)
+        (setf (cadr i) (evaluate (cadr i) env)))
+    (setf alist (cons (cons (car i) (cadr i)) alist)))
+  (setf env (append alist env))
+  (eprogn (cddr e) env))
+  
+(defun lookup (e env)
+  (let ((v (assoc e env)))
+    (if v (cdr v)
+        (wrong "Unbound-variable: ~S~%" e))))
+
+(defun update! (var env value)
+  (let ((kv (assoc var env)))
+    (setf (cdr kv) value)))
+
+(defun eprogn (el env)
+  (let ((result (evaluate (car el) env)))
+    (dolist (e (cdr el) result)
+      (setf result (evaluate e env)))))
+
+(defun procedure? (fn)
+  (or (eql fn '+)
+      (eql fn '-)
+      (eql fn '*)
+      (eql fn '/)))
+
+(defun wrong (m &optional v)
+  (format t m v))
+
+(defun check-arg-types (fn args)
+  (and (procedure? fn)
+       (numberp (first args))
+       (numberp (second args))))
+
+(defun invoke (fn args)
+  (if (procedure? fn)
+      (apply fn args)
+      (wrong "Could not evaluate function: ~S" fn)))
+
+(defun evlist (lis env)
+  (when lis
+    (let ((a (evaluate (car lis) env)))
+      (cons a (evlist (cdr lis) env)))))
+
+(defun constant? (s)
+  (or (eql s 'nil)
+      (eql s 't)
+      (stringp s)))
+
+(defun symbol? (s)
+  (and (symbolp s) (not (procedure? s)) (not (constant? s))))
+
+(defun valid-args-num? (lst num)
+  (let* ((len (length lst)) (res (= len num)))
+    (list res len num)))
+
+(defun valid-args? (lst)
+  (if (eql (type-of lst) 'cons)
+      (case (car lst)
+        ((setf) (valid-args-num? lst 3))
+        ((if) (valid-args-num? lst 4))
+        ((let)
+         (progn
+           (loop for item in (cadr lst) do
+                 (let ((res (valid-args-num? item 2)))
+                   (if (not (car res))
+                       (return-from valid-args? (list nil (cadr res) 2)))))
+           '(t 2 2)))
+        (t (let ((len (length lst)))
+             (list t len len))))
+      '(t 0 0)))
